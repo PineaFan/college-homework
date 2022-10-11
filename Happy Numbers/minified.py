197bac6c73b5ffb3c5d08da492f3d4e7d3176ea5
PineappleFan <pineapplefanyt@gmail.com>
2022-10-11 21:46:59 +0100

    Update minified.py

--- Happy Numbers/minified.py
+++ Happy Numbers/minified.py
@@ -1,1 +1,1 @@
-for n in range(30):print(n,(lambda n:f"{'H'if 1 in [n]+[n:=sum(int(i)**2 for i in list(str(n)))for _ in range(n+1)]else'U'}")(n))
+print(n,(lambda n:'H'if 1 in[n]+[n:=sum(int(i)**2for i in list(str(n)))for _ in range(n+1)]else'U')(n))

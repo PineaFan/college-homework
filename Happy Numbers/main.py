aeaf7b9bddafea381802a1df0e11e381591b0aad
PineappleFan <pineapplefanyt@gmail.com>
2022-10-14 21:49:41 +0100

    Update main.py

--- Happy Numbers/main.py
+++ Happy Numbers/main.py
@@ -3,7 +3,7 @@ n = 1
 while happy < 8:
     history = [n]
     while len(history) == len(set(history)):
-        n = sum(int(i)**2 for i in list(str(n)))
+        n = sum(int(i)**2 for i in str(n))
         history += [n]
     if 1 in history:
         print(f"{history[0]} Happy")
